6380dd9cd4afc728b029db8db614e37808e85e09
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-05-18 00:58:15 -0300

    config

--- routes/TeamRoutes.js
+++ routes/TeamRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const TeamController = require('../controllers/TeamController')
 
-router.post('/teams', TeamController.createTeam)
+// router.post('/teams', TeamController.createTeam)
 router.get('/equipes', TeamController.equipes)
 
 module.exports = router
