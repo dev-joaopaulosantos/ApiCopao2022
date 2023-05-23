4b26d49c985aff91649f1925bf59f64f5a901385
João Paulo Santos <83318297+dev-joaopaulosantos@users.noreply.github.com>
2023-05-23 00:44:50 -0300

    Update InfoRoutes.js

--- routes/InfoRoutes.js
+++ routes/InfoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const InfoController = require('../controllers/InfoController')
 
-router.post('/infos', InfoController.createInfo)
+// router.post('/infos', InfoController.createInfo)
 router.get('/infos', InfoController.infos)
 
 module.exports = router
